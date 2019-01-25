d04ca52a103668fe9b5ca13c1fec977d3b8a040b
xzhu <skypigr@gmail.com>
2019-01-25 15:16:22 -0800

    add NonPreemptiveHighestPriorityFirst(No Aging)

---
+++ src/com/scu/coen383/team2/scheduling/NonpreemptiveHighestPriorityFirst.java
@@ -0,0 +1,16 @@
+package com.scu.coen383.team2.scheduling;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class NonpreemptiveHighestPriorityFirst extends ScheduleBase {
+
+    @Override
+    public Queue<Process> schedule(PriorityQueue<Process> q){
+        Queue<Process> scheduledQueue = new LinkedList<>();
+        // todo: implement HPF here
+
+        return scheduledQueue;
+    }
+}
