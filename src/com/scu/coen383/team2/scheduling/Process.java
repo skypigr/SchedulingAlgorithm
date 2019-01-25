5263b66208ac4f9d7d9cd05b2daefb19350ab3b3
xzhu <skypigr@gmail.com>
2019-01-24 17:08:41 -0800

    revise

--- src/com/scu/coen383/team2/scheduling/Process.java
+++ src/com/scu/coen383/team2/scheduling/Process.java
@@ -2,20 +2,18 @@ package com.scu.coen383.team2.scheduling;
 
 public class Process implements Comparable<Process> {
 
-    private char _name;             // pcocess name
-    private int _priority;          // priority 1,2,3,4
-    private float _arrivalTime;    // arrival time 0 - 99
-    private int _startTime;         // start time
-    private float _serviceTime;    // expected total run time 0.1 - 10
+    private char    _name;              // pcocess name
+    private int     _priority;          // priority 1,2,3,4
+    private float   _arrivalTime;       // arrival time 0 - 99
+    private int     _startTime;         // start time
+    private float   _serviceTime;       // expected total run time 0.1 - 10
 
-    public int getPriority()        { return _priority; }
-    public char getName()           { return _name; }
-    public float getArrivalTime()   { return _arrivalTime; }
-    public float getServiceTime()   { return _serviceTime; }
-    public int getServiceQuanta()   {return (int)Math.ceil(_serviceTime);}
-    public int getStartTime()       { return _startTime; }
-
-
+    public int      getPriority()       { return _priority; }
+    public char     getName()           { return _name; }
+    public float    getArrivalTime()    { return _arrivalTime; }
+    public float    getServiceTime()    { return _serviceTime; }
+    public int      getServiceQuanta()  {return (int)Math.ceil(_serviceTime);}
+    public int      getStartTime()      { return _startTime; }
 
 
     Process(char name, float arrival_time, float service_time, int priority, int start_time) {
