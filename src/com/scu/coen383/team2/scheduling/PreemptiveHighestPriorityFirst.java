bdab3d798ffd0184c3b532aa66fde39686724f00
xzhu <skypigr@gmail.com>
2019-01-25 17:05:23 -0800

    add PreemptiveHighestPriorityFirst

---
+++ src/com/scu/coen383/team2/scheduling/PreemptiveHighestPriorityFirst.java
@@ -0,0 +1,13 @@
+package com.scu.coen383.team2.scheduling;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PreemptiveHighestPriorityFirst extends ScheduleBase {
+    public Queue<Process> schedule(PriorityQueue<Process> initialQueue) {
+        Queue<Process> scheduledQueue = new LinkedList<>();
+
+        return scheduledQueue;
+    }
+}
